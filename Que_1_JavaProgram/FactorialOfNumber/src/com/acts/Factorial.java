425a02184ffe3239a81707f6e95bacd570cb1aaf
9PratikPatil <pratikpatil0906@gmail.com>
2023-02-27 16:45:59 +0530

    first commit for adding all files

---
+++ Que_1_JavaProgram/FactorialOfNumber/src/com/acts/Factorial.java
@@ -0,0 +1,20 @@
+package com.acts;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)){
+			System.out.println("Enter the number to find its factorial ");
+			int num=sc.nextInt();
+			
+			int factorial=1;
+			while(num != 0) {
+				factorial = factorial * num;
+				num--;
+			}
+			 System.out.println("Factorial of a given number is -->  " + factorial);		
+		}
+	}
+}
